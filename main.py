f277a44ed60864c183be43e39ffaf2222e24f9b5
wolfgang <wolfgangjblack@gmail.com>
2024-07-09 13:38:48 -0700

    updated trainer to work

--- main.py
+++ main.py
@@ -62,50 +62,24 @@ def custom_collator(batch):
 
 ## instantiate model
 model = MultimodalModel(config)
-def compute_metrics(eval_pred):
-    print("Computing metrics...")
-    try:
-        logits, labels = eval_pred
-        if isinstance(labels, dict):
-            labels = labels['label']  # Adjust this key if necessary
-        print(f"Logits shape: {logits.shape}, Labels shape: {labels.shape}")
-        predictions = np.argmax(logits, axis=-1)
-        eval_loss = np.mean(logits - labels)
-        accuracy = np.mean(predictions == labels)
-        f1 = f1_score(labels, predictions, average='weighted')
-        print(f"Computed metrics - Accuracy: {accuracy}, F1: {f1}")
-        return {
-            "eval_acc": accuracy,
-            "eval_f1": f1,
-            "eval_loss": eval_loss
-        }
-    except Exception as e:
-        print(f"Error in compute_metrics: {e}")
-        import traceback
-        traceback.print_exc()
-        return {}
     
-## instantiate trainer
+## instantiate trainer - note: this trainer does not allow use to target specific metrics. We'll write our own training loop
 training_args = TrainingArguments(
     output_dir='./results',          # output directory
     num_train_epochs=2,
     per_device_train_batch_size=32,
-    per_device_eval_batch_size=8,
+    per_device_eval_batch_size=32,
     warmup_steps=500,                # number of warmup steps for learning rate scheduler
     weight_decay=0.01,               # strength of weight decay
     logging_dir='./logs',            # directory for storing logs
-    logging_steps=10,
+    logging_steps=25,
     evaluation_strategy="steps",
     eval_steps=500,
     save_steps=500,
-    load_best_model_at_end=True,
     save_safetensors=True,  # Ensure this is supported by your Transformers version
-    metric_for_best_model="eval_f1",  # Choose eval_f1 or eval_acc
-    greater_is_better=True,  # Set to True because higher F1/accuracy is better
     report_to='all',  # Enable detailed logging to all available loggers
     disable_tqdm=False,  # Ensure progress bars are shown
     logging_first_step=True,  # Log the first step
-
 )
 
 
@@ -115,7 +89,6 @@ trainer = Trainer(
     train_dataset=training_data,
     eval_dataset=validation_data,
     data_collator=custom_collator,
-    compute_metrics=compute_metrics,
 )
 
 trainer.train()
